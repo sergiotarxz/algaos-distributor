3c15f1a4b7acc070d1459143853c6e0150ca7ad7
Sergio <sergiotarxz@posteo.net>
2026-09-13 12:42:31 +0200

    First version with everything needed to implement cohort support
    In the future.

---
+++ lib/AlgaOS/Distributor.pm
@@ -0,0 +1,32 @@
+package AlgaOS::Distributor;
+
+use v5.40.0;
+use strict;
+use warnings;
+
+use Mojo::Base 'Mojolicious', -signatures;
+
+sub startup ($self) {
+    my $config = $self->plugin('NotYAMLConfig');
+
+    $self->secrets( $config->{secrets} );
+
+    my $r = $self->routes;
+
+    $r->add_type( machine_id => qr/[0-9a-f]{32}/ );
+    $r->add_condition(
+        relative_path => sub ( $route, $c, $captures, @extra ) {
+            my $path = $c->param('path_binpkg') // '';
+
+            return $path !~ m{(?:^|/)\.\.(?:/|$)};
+        }
+    );
+
+    # Router
+    $r->get('/')->to('Root#welcome');
+    $r->get('/:machine_id<machine_id>/webrsync.tar.bz2')->to('Root#webrsync');
+    $r->get('/:machine_id<machine_id>/binpkg/*path_binpkg')
+      ->requires( relative_path => 1 )
+      ->to('Root#binpkg');
+}
+1;
---
+++ lib/AlgaOS/Distributor/Controller/Root.pm
@@ -0,0 +1,46 @@
+package AlgaOS::Distributor::Controller::Root;
+
+use v5.40.0;
+use strict;
+use warnings;
+
+use Mojo::Base 'Mojolicious::Controller', -signatures;
+use Mojo::File qw/path/;
+
+# This action will render a template
+sub welcome ($self) {
+
+    # Render template "example/welcome.html.ep" with message
+    $self->render( text => 'hola' );
+}
+
+sub webrsync ($self) {
+    my $machine_id = $self->param('machine_id');
+    my $tag        = 'latest';
+
+    my $file = "/var/www/algaos/downloads/webrsync-$tag.tar.bz2";
+
+    return $self->reply->not_found unless -f $file;
+
+    $self->reply->file($file);
+}
+
+sub binpkg ($self) {
+    my $tag = 'latest';
+
+    my $root = path("/var/www/algaos/downloads/binpkg-algaos-$tag")->realpath;
+
+    my $relative = $self->param('path_binpkg');
+    say $relative;
+    my $file     = $root->child($relative)->realpath;
+    say $file;
+
+    return $self->reply->not_found
+      unless $file && -f $file;
+
+    return $self->reply->not_found
+      unless $file->to_string =~ m{\A\Q$root\E/};
+
+    $self->reply->file($file);
+}
+1;
